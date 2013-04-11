0034afdc3e33c3b2f78474282746b22af9a0e2a7
Korbanda11as <ijatalalai@gmail.com>
2013-04-11 10:54:46 -0700

    connect to S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,14 @@ Omrails::Application.configure do
   
   #In production, :host should be set to the actual host of your application.
 config.action_mailer.default_url_options = { :host => 'http://radiant-atoll-8543.herokuapp.com/' }
+
+#required for Amazon s3 configuration with paperclip
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
